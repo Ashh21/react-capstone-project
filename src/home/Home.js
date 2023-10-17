77dc0fd8fbb0a4fa3f4bcf381aa668c7641fc47d
Ashh21 <106957781+Ashh21@users.noreply.github.com>
2023-10-17 21:13:49 +0530

    Update Home.js

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,15 +1,31 @@
-import React, { } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css"
 import { UserProfile } from './UserProfile'
 import { WeatherCard } from './WeatherCard'
 import { NotesCard } from './NotesCard'
-import { NewsCard } from './NewsCard'
+// import { NewsCard } from './NewsCard'
 import { useNavigate } from 'react-router-dom'
 import { TimerCard } from './TimerCard'
 
 
 const Home = () => {
     const navigate = useNavigate()
+    const [newsData, setNewsData] = useState("")
+
+    useEffect(() => {
+        try {
+            const fetchData = async () => {
+                const data = await fetch("https://newsdata.io/api/1/news?apikey=pub_31353df17a9dc5ae288e976e834ff4579ab23&q=pizza")
+                const json = await data.json();
+                setNewsData(json?.results[7])
+                console.log(json)
+            }
+            fetchData()
+        }
+        catch (e) {
+
+        }
+    }, [])
     const browseBtnHandler = () => {
         navigate("/entertainmentPage")
     }
@@ -23,7 +39,19 @@ const Home = () => {
                 <NotesCard />
             </div>
             <div class="grid-item c">
-                <NewsCard />
+                {/* <NewsCard /> */}
+                <div className='news'>
+            <div className='news-img-div'>
+                <img className='news-img' src={newsData?.image_url} alt="logo" />
+                <div className='news-title-div'>
+                    <h1>{newsData?.title}</h1>
+                    <p> {newsData?.pubDate} </p>
+                </div>
+            </div>
+            <div className='new-content-div'>
+                <h3> {newsData?.content?.split("...")?.[0]} ... </h3>
+            </div>
+        </div>
             </div>
             <div class="grid-item d">
                 <WeatherCard />
